f165bc0fa6297b25c78978f2584287d97654a714
NhonoLama <wadalama111@gmail.com>
2025-03-03 12:33:37 +0545

    gesture added

--- screens/AnimationScreen.tsx
+++ screens/AnimationScreen.tsx
@@ -1,23 +1,45 @@
-import {Button, View} from 'react-native';
-import Animated, {useSharedValue} from 'react-native-reanimated';
+import {useEffect} from 'react';
+import {Button, StyleSheet, View} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSequence,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function AnimationScreen() {
   const width = useSharedValue<number>(100);
+  const translateX = useSharedValue<number>(10);
 
   const handlePress = () => {
-    width.value = width.value + 50;
+    width.value = withSpring(width.value + 50);
   };
+
+  useEffect(() => {
+    translateX.value = withDelay(
+      500,
+      withSequence(
+        withTiming(translateX.value - 20, {duration: 150}),
+        withRepeat(withTiming(translateX.value + 20, {duration: 300}), 5, true),
+        withTiming(0, {duration: 150}),
+      ),
+    );
+  }, []);
+
+  const animateMe = useAnimatedStyle(() => ({
+    transform: [{translateX: translateX.value}],
+  }));
 
   return (
     <View style={{flex: 1, alignItems: 'center'}}>
-      <Animated.View
-        style={{
-          width,
-          height: 100,
-          backgroundColor: 'violet',
-        }}
-      />
+      <Animated.View style={[styles.box, {width}, animateMe]} />
       <Button onPress={handlePress} title="Click me" />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  box: {height: 100, backgroundColor: 'violet', margin: 20},
+});
--- screens/SwipeScreen.tsx
+++ screens/SwipeScreen.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
 const SwipeScreen = () => {
-  return <View></View>;
+  const pressed = useSharedValue<boolean>(false);
+  const offset = useSharedValue<number>(0);
+
+  const pan = Gesture.Pan()
+    .onBegin(() => {
+      pressed.value = true;
+      console.log('begin');
+    })
+    .onChange(e => {
+      offset.value = e.translationX;
+    })
+    .onEnd(() => {
+      pressed.value = false;
+      offset.value = withSpring(0);
+      console.log('ended');
+    });
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    backgroundColor: pressed.value ? '#FFE04B' : '#B58DF1',
+    width: withTiming(pressed.value ? 120 : 100),
+    height: withTiming(pressed.value ? 120 : 100),
+    transform: [{translateX: offset.value}],
+  }));
+
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <GestureDetector gesture={pan}>
+          <Animated.View style={[styles.circle, animatedStyles]} />
+        </GestureDetector>
+        <Text style={{marginTop: 20}}>Tap and Pan Gesture</Text>
+      </View>
+    </GestureHandlerRootView>
+  );
 };
+
+const styles = StyleSheet.create({
+  circle: {
+    borderRadius: 100,
+  },
+});
 export default SwipeScreen;
